0d96b4d59e5507ea9c15f8e5e536b061ea31031f
Liza Carvelli <liza@carvel.li>
2024-10-19 10:31:45 +0200

    Add new actions for NIN quests

--- Questionable.Model/Questing/Converter/ActionConverter.cs
+++ Questionable.Model/Questing/Converter/ActionConverter.cs
@@ -35,5 +35,8 @@ public sealed class ActionConverter() : EnumConverter<EAction>(Values)
         { EAction.BlueGulal, "Blue Gulal" },
         { EAction.ElectrixFlux, "Electric Flux" },
         { EAction.HopStep, "Hop-step" },
+        { EAction.FumaShuriken, "Fuma Shuriken" },
+        { EAction.Katon, "Katon" },
+        { EAction.Raiton, "Raiton" },
     };
 }
