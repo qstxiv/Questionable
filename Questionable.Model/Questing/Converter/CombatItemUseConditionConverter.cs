b5e1014e212ac1ae03dfddc37b7bd33f3f4c690b
Liza Carvelli <liza@carvel.li>
2025-01-19 22:58:14 +0100

    Add logic for using items on the Spotted Mudpuppy in 'Factual Folklore'

--- Questionable.Model/Questing/Converter/CombatItemUseConditionConverter.cs
+++ Questionable.Model/Questing/Converter/CombatItemUseConditionConverter.cs
@@ -9,5 +9,6 @@ public sealed class CombatItemUseConditionConverter() : EnumConverter<ECombatIte
     {
         { ECombatItemUseCondition.Incapacitated, "Incapacitated" },
         { ECombatItemUseCondition.HealthPercent, "Health%" },
+        { ECombatItemUseCondition.MissingStatus, "MissingStatus" },
     };
 }
